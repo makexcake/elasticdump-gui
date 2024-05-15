e143fa99308262c0180130836f7189cb52a089cf
Vitaly Nudelman <vitaly.nudelman@comm-it.com>
2024-05-15 15:32:46 +0300

    updated default filter to client side

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const { exec } = require('child_process');
 const path = require('path');
+const fs = require('fs');
+// const updateFilterTimestamp = require('./utils/updateFilterTimestamp');
 
-
+const filterFilePath = 'filters/filter.json';
 const app = express();
-
 app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'static')));
+
+app.get('/default-filter', (req, res) => {
+  res.sendFile(path.join(__dirname, 'filters', 'default-filter.json'), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -19,13 +28,24 @@ app.post('/extract-logs', (req, res) => {
   const endpoint = req.body.endpoint || process.env.ENDPOINT;
   const indexName = req.body.indexName || process.env.INDEX_NAME;
   const outputFilename = req.body.outputFilename;
-  const filter = req.body.filter;
+  let filter = req.body.filter;
+
   const useHttps = scheme === 'https';
+
+  // If no filter is provided in gui, use the default filter
+  if (filter == null || filter === '') {
+    const existingFilterJson = fs.readFileSync(filterFilePath, 'utf8');
+    const existingFilter = JSON.parse(existingFilterJson);
+    // const updatedFilter = updateFilterTimestamp(existingFilter);
+    const updatedFilterVariable = updatedFilter;
+    console.log('Updated Filter:', updatedFilterVariable);
+    filter = JSON.stringify(updatedFilterVariable);
+  }
 
   const commands = [
     'cd /app/imported-indices',
     `elasticdump --output=${outputFilename}-mapping.json --input=${scheme}://${username}:${password}@${endpoint}/${indexName} --type=mapping --limit=10000`,
-    `elasticdump --output=${outputFilename}-data.json --input=${scheme}://${username}:${password}@${endpoint}/${indexName} --type=data --limit=10000 --searchBody='${filter}'`,
+    `elasticdump --output=${outputFilename}-data.json --input=${scheme}://${username}:${password}@${endpoint}/${indexName} --type=data --limit=10000 --searchBody='${JSON.stringify(filter)}'`,
   ];
 
   exec(commands.join(' && '), (error, stdout, stderr) => {
